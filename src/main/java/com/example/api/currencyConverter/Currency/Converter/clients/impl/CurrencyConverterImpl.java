8a65977de963745d2a02d0e5917a0b6df7fdb624
sauravkumarburnwal36 <sauravkumarburnwal36@gmail.com>
2024-12-13 11:34:14 +0530

    prod ready features

--- src/main/java/com/example/api/currencyConverter/Currency/Converter/clients/impl/CurrencyConverterImpl.java
+++ src/main/java/com/example/api/currencyConverter/Currency/Converter/clients/impl/CurrencyConverterImpl.java
@@ -3,6 +3,7 @@ package com.example.api.currencyConverter.Currency.Converter.clients.impl;
 import com.example.api.currencyConverter.Currency.Converter.clients.CurrencyConverter;
 import com.example.api.currencyConverter.Currency.Converter.dto.CurrencyConverterDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,9 @@ import org.springframework.web.client.RestClient;
 public class CurrencyConverterImpl implements CurrencyConverter {
     private final RestClient restClient;
 
+    @Value("${base.api.key}")
+    private String apiKey;
+
     @Override
     public Double getCurrencyConverter(String fromCurrency, String toCurrency, Long units) {
         CurrencyConverterDTO currencyConverterDTO=restClient.get()
@@ -22,7 +26,7 @@ public class CurrencyConverterImpl implements CurrencyConverter {
                         .queryParam("currencies",toCurrency)
                         .queryParam("base_currency",fromCurrency)
                         .build())
-                .header("apiKey","fca_live_YerebnTVlmIOUgP3hDqkupazKQhfmU15fBFhbq6V")
+                .header("apiKey",apiKey)
                 .retrieve()
                 .onStatus(HttpStatusCode::is4xxClientError,(req,res)->{
                     throw new HttpClientErrorException(res.getStatusCode(),res.getBody().toString());
